1db64319f56450d9df7a4c935f15a1e37072a532
massqeen <62159695+massqeen@users.noreply.github.com>
2025-02-18 09:18:07 +0100

    Initial commit

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+
+declare module 'vuex' {
+  export * from 'vuex/types/index.d.ts'
+  export { Store, StoreOptions } from 'vuex/types/index.d.ts'
+}
+
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import GameView from '@/views/GameView.vue'
+</script>
+
+<template>
+  <GameView />
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
